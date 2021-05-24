0b25176dca8a501edcc5b0d58a5573ead0567e8e
Gabriel Ramos <gabri.elramos@live.com>
2021-05-24 00:40:47 -0300

    Implemented layout

---
+++ app/src/main/java/br/com/gabrielramos/desafiosouthsystem/presenter/features/details/DetailActivity.kt
@@ -0,0 +1,12 @@
+package br.com.gabrielramos.desafiosouthsystem.presenter.features.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.gabrielramos.desafiosouthsystem.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
